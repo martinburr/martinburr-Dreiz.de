0d8329bb913745298ccdc3199a6aac593f9197f7
Martin Burr <68325138+martinburr@users.noreply.github.com>
2021-07-03 19:54:52 +0200

    Delete style.js

--- style.js
+++
@@ -1,4 +0,0 @@
-gsap.timeline()
-    .fromTo(".header_img", { duration: 0, opacity: 0 }, { duration: 2, opacity: 1 })
-    .fromTo("#logo", { opacity: 0 }, { duration: 3, opacity: 1 })
-    .fromTo(".nav_links", { opacity: 0 }, { duration: 3, opacity: 1 }, "-=3" )
