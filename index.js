3d90bda6f21d95329b391b2993baa2a6167497b3
Martin Burr <68325138+martinburr@users.noreply.github.com>
2022-02-13 15:25:54 +0100

    new
    13.2.2022

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 gsap.timeline()
-    .fromTo(".header_img", { duration: 0, opacity: 0 }, { duration: 2, opacity: 1 })
-    .fromTo("#logo", { opacity: 0 }, { duration: 3, opacity: 1 })
+
+    .fromTo("header", { duration: 0, opacity: 0 }, { duration: 3, opacity: 1 })
+    .fromTo(".logo", { opacity: 0 }, { duration: 3, opacity: 1 })
     .fromTo(".nav_links", { opacity: 0 }, { duration: 3, opacity: 1 }, "-=3" )
